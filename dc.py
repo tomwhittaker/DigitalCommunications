67afd23a9941bcc827d973474d949f1072074f9e
tomwhittaker <thomas.j.whittaker@durham.ac.uk>
2017-03-15 16:55:50 +0000

    repeated squares gives right answer but wrong intermediates

--- dc.py
+++ dc.py
@@ -31,8 +31,52 @@ def VigenereDecode(message,key):
         encoded=encoded+alphabet[pos]
     return encoded
 
-
-encoded = VigenereDecode('ZZHQRPCOYRPGQOPZPBEHVXHAYWJO','YOHOHO')
-
-if encoded == 'BLACKBEARDISSAILINGTOJAMAICA':
-    print "works"
+def repeatedSquare(number,power,mod):
+    halfPower=power/2
+    powers=[]
+    powerOfTwo=[]
+    posistionFinder=[]
+    current=1
+    value=number
+    while current<power:
+        value=value%mod
+        powers.append(value)
+        powerOfTwo.append(current)
+        posistionFinder.append(current)
+        current=current*2
+        value=value*value
+        print  "",number,"^",current,"=",value
+    used=[]
+    while len(powerOfTwo) !=0:
+        num = powerOfTwo.pop()
+        if power-num>=0:
+            power=power-num
+            used.append(num)
+    answer=1
+    for x in used:
+        pos = posistionFinder.index(x)
+        answer =answer*powers[pos]%mod
+    return answer
+#Q1
+encoded = VigenereEncode('LETSSAILFORTHESPANISHMAIN','PIECESOFEIGHT')
+#q2
+decoded= VigenereDecode('ZVTVKGVBLNWYJVCLBOOHSSKFIGWYOEDNZ','GOLDCOINS')
+#q3
+encoded = VigenereDecode('IXYVAGYZLHMRCLAGUGXEQSMFXAZTHFCKNSGXINVHWDOOUPVUNYIZFMKYTOVKKEADYZPZGXWJFVSXGMXAZMHQTRPYGIXTESRVIGWWSLTHXIZIDPEVSSDRHIKKGDTYHWICSYTOIWPUBZNFVEZCHWWGHUAJBBXIJTGLRJHGLHXRNJANPNPZW\
+ZYCIPHPYOANIPHOBKKRPYJHVTTEHROJYQCDTRDOWUKGCEEPLVNZSRCMTOUY\
+LHWNHXGHNMWJEMHCIOHKNIHXSOTMDRSCBQYZYNU','KPKWETQKAODLZMERBOCAPNEERINWHQYBUOQUWTXMKIIBLNISOQAFRQHFHBE\
+YXUPDMIMHEJNURXYQCXMULOVEKKXZZQWUUIBVSLMDJYQGBEVBIXDSJVXVPM\
+YAAZROGEBGWIEVHLADXKRUIUYZDNCJTTKXDCHKNWGDNCKQGCBZVZNJPOFDY\
+WWYRDMKFHKXFMFRGLMKHRWHFRJVNSGAQJHNCBYGSCEOPDVRRPPFWLOGUSRH\
+ZRIIAKYGZBJVPPQLRMMFGFBXTSMBJFLBOAWBKCD')
+answer = VigenereDecode('YAGNKAIHHXBZDQPTWHMSHWINSAZDWTJXNRTWAISMEPXCAKFGSPINWPZTEAK\
+MFOLSKKRYGHYGCELHQHCOUFFMBDWGHVUGYEWHJYOUDTJJJKLHYLPENAOOUK\
+HOXIFSDSUNACTFCPGKLTAINMULRBNXBUMTCMYPAXLQDLAJMFPSDKEKEOKNR\
+RHQPXDKXMHEPWJBINLDIZTOYPQCIAMBXLIJZDKQRVOFTNRQNGIYOOBSXKZE\
+BHCLYNUTALHFXZVNZNFJZGOSBKCPLEKFOKIEWYX',encoded)
+#Q4i
+print repeatedSquare(17,54,139)
+#q4ii
+print repeatedSquare(2345,65531,265189)
+#q4iii
+print repeatedSquare(4733459,65537,75968647)
